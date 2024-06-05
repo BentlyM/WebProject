c0e923af3083566a88fcf47e9b6bef7e40241401
BentlyM <metayerbently@gmail.com>
2024-06-05 11:17:35 -0400

    added nested object arrays to assign values

--- web/script.js
+++ web/script.js
@@ -1,18 +1,44 @@
-// const add = (x , y) => x + y;
-// const sub = (x , y) => x - y;
-// const multi = (x , y) => x * y;
-// const divide = (x , y) => x / y;
+const division = document.getElementById('division');
+const multiply = document.getElementById('multiplication');
+const subtraction = document.getElementById('subtraction');
+const addition = document.getElementById('addition');
+const equal = document.getElementById('equal');
 
+const buttons = [
+  { value: '1' },
+  { value: '2' },
+  { value: '3' },
+  { value: '4' },
+  { value: '5' },
+  { value: '6' },
+  { value: '7' },
+  { value: '8' },
+  { value: '9' },
+];
+
+const operations = [
+  {value: '/'},
+  {value: '*'},
+  {value: '-'},
+  {value: '+'}
+]
+
+const add = (x , y) => x + y;
+const sub = (x , y) => x - y;
+const multi = (x , y) => x * y;
+const divide = (x , y) => x / y;
+let number1 , number2, operator; 
 
 const createGridLayout = () => {
     const container = document.getElementById('numbers');
-    const child = container.children;
-    for (let i = 0; i < 9; ++i) {
-        const element = document.createElement('div');
-        element.className = 'btn';
-        element.innerHTML = i + 1;
-        container.append(element);
-      }
+    // const children = container.childNodes; // for grab children node values
+    buttons.forEach((button) => {
+      const buttonElement = document.createElement('div');
+      buttonElement.className = 'btn';
+      buttonElement.textContent = button.value;
+      buttonElement.value = button.value;
+      container.appendChild(buttonElement);
+    });
 }
 
 const operate = (x, y, op) => {
