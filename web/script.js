7bd9357112585adf15a1e8611e401c532b818f45
BentlyM <metayerbently@gmail.com>
2024-06-05 20:40:49 -0400

    moved the opertions object

--- web/script.js
+++ web/script.js
@@ -1,8 +1,18 @@
-const division = document.getElementById('division');
-const multiply = document.getElementById('multiplication');
-const subtraction = document.getElementById('subtraction');
-const addition = document.getElementById('addition');
-const equal = document.getElementById('equal');
+const operations = [
+  {value: '/'},
+  {value: '*'},
+  {value: '-'},
+  {value: '+'},
+  {value: '='}
+]
+
+const division = document.getElementById('division').value = operations[0].value;;
+const multiply = document.getElementById('multiplication').value = operations[1].value;
+const subtraction = document.getElementById('subtraction').value = operations[2].value;
+const addition = document.getElementById('addition').value = operations[3].value;
+const equal = document.getElementById('equal').value = operations[4].value;
+const digit = document.getElementById('digit');
+let number1 , number2, operator; 
 
 const buttons = [
   { value: '1' },
@@ -16,20 +26,12 @@ const buttons = [
   { value: '9' },
 ];
 
-const operations = [
-  {value: '/'},
-  {value: '*'},
-  {value: '-'},
-  {value: '+'}
-]
-
 const add = (x , y) => x + y;
 const sub = (x , y) => x - y;
 const multi = (x , y) => x * y;
 const divide = (x , y) => x / y;
-let number1 , number2, operator; 
 
-const createGridLayout = () => {
+const createNumberLayout = () => {
     const container = document.getElementById('numbers');
     // const children = container.childNodes; // for grab children node values
     buttons.forEach((button) => {
@@ -38,7 +40,11 @@ const createGridLayout = () => {
       buttonElement.textContent = button.value;
       buttonElement.value = button.value;
       container.appendChild(buttonElement);
-    });
+      buttonElement.onclick = () => {
+        if(digit.innerHTML === '0'){digit.innerHTML = ''}
+        digit.innerHTML += buttonElement.value;
+      };
+  });
 }
 
 const operate = (x, y, op) => {
@@ -56,4 +62,4 @@ const operate = (x, y, op) => {
     }
   };
 
-  createGridLayout();
+  createNumberLayout();
